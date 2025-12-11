d85cce70304bcb9656018192568afd276710dc99
Gregory Bowne <gbowne144@gmail.com>
2025-12-10 23:26:58 -0800

    Create 002_create_message.sql

---
+++ database/migrations/002_create_message.sql
@@ -0,0 +1,7 @@
+CREATE TABLE messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    text TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
