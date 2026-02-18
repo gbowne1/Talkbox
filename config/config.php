95f5b405540c43bd6bd9042fd59ba5b0db727e87
Gregory Bowne <gbowne144@gmail.com>
2026-02-18 10:58:27 -0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,10 +1,10 @@
 <?php
 return [
-    'driver'   => 'mysql',   // mysql | mariadb | pgsql | sqlite | mongodb
+    'driver'   => 'mysql',
     'host'     => '127.0.0.1',
     'port'     => 3306,
     'dbname'   => 'talkbox',
     'username' => 'root',
     'password' => 'secret',
-    'path'     => __DIR__ . '/../data/talkbox.sqlite' // for SQLite
 ];
+
